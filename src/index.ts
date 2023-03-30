321118caa36b3b5df4a048b23f1c68789c7093b0
David Peng <davipondev@gmail.com>
2023-03-30 22:16:36 +0800

    feat: adapt to the new esbuild entryPoints API (#75)
    * feat(BREAKING): adapt to the new esbuild entryPoints API
    
    * doc: update README.md

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,81 @@
 import path from 'node:path'
 import { readFile } from 'node:fs/promises'
-import type { Plugin } from 'esbuild'
+import type { Plugin, BuildOptions } from 'esbuild'
+
+type NewEntrypointsType = Extract<
+  BuildOptions['entryPoints'],
+  { in: string; out: string }[]
+>
+
+/**
+ * Check if entrypoints is an array of strings
+ * @param entryPoints
+ * @returns
+ */
+function isStringArray(
+  entryPoints: BuildOptions['entryPoints']
+): entryPoints is string[] {
+  if (
+    Array.isArray(entryPoints) &&
+    entryPoints.some((entrypoint) => typeof entrypoint === 'string')
+  )
+    return true
+  return false
+}
+
+/**
+ * Transform from string[] to Record<string, string>
+ * @param entryPoints
+ * @param outbase
+ * @returns
+ */
+function transformToObject(
+  entryPoints: string[],
+  outbase: string | undefined
+): Record<string, string> {
+  const separator = entryPoints[0].includes('\\')
+    ? path.win32.sep
+    : path.posix.sep
+
+  if (!outbase) {
+    const hierarchy = entryPoints[0].split(separator)
+    let i = 0
+    outbase = ''
+    let nextOutbase = ''
+    do {
+      outbase = nextOutbase
+      i++
+      nextOutbase = hierarchy.slice(0, i).join(separator)
+    } while (
+      entryPoints.every((entrypoint: string) =>
+        entrypoint.startsWith(`${nextOutbase}${separator}`)
+      )
+    )
+  }
+  const newEntrypoints: Record<string, string> = {}
+  for (const entrypoint of entryPoints) {
+    const destination = (
+      outbase ? entrypoint.replace(`${outbase}${separator}`, '') : entrypoint
+    ).replace(/.(js|ts)$/, '')
+    newEntrypoints[destination] = entrypoint
+  }
+  return newEntrypoints
+}
+
+/**
+ * Transform from Record<string, string> to { in: string, out: string }[]
+ * @param entryPoints
+ * @returns
+ */
+function transformToNewEntryPointsType(
+  entryPoints: Record<string, string>
+): NewEntrypointsType {
+  const newEntrypointsType: NewEntrypointsType = []
+  for (const [key, value] of Object.entries(entryPoints)) {
+    newEntrypointsType.push({ in: value, out: key })
+  }
+  return newEntrypointsType
+}
 
 /**
  * A pino plugin for esbuild
@@ -34,7 +109,7 @@ import type { Plugin } from 'esbuild'
  * ```
  */
 export default function esbuildPluginPino({
-  transports
+  transports = []
 }: {
   transports: string[]
 }): Plugin {
@@ -44,51 +119,46 @@ export default function esbuildPluginPino({
       const pino = path.dirname(require.resolve('pino'))
       const threadStream = path.dirname(require.resolve('thread-stream'))
 
-      let entrypoints = currentBuild.initialOptions.entryPoints
-      if (Array.isArray(entrypoints)) {
-        const separator = entrypoints[0].includes('\\')
-          ? path.win32.sep
-          : path.posix.sep
-        let outbase = currentBuild.initialOptions.outbase
-        if (!outbase) {
-          const hierarchy = entrypoints[0].split(separator)
-          let i = 0
-          outbase = ''
-          let nextOutbase = ''
-          do {
-            outbase = nextOutbase
-            i++
-            nextOutbase = hierarchy.slice(0, i).join(separator)
-          } while (
-            entrypoints.every((e) => e.startsWith(`${nextOutbase}${separator}`))
-          )
-        }
-        const newEntrypoints: Record<string, string> = {}
-        for (const entrypoint of entrypoints) {
-          const destination = (
-            outbase
-              ? entrypoint.replace(`${outbase}${separator}`, '')
-              : entrypoint
-          ).replace(/.(js|ts)$/, '')
-          newEntrypoints[destination] = entrypoint
-        }
-        entrypoints = newEntrypoints
-      }
-
-      const customEntrypoints = {
+      const { entryPoints, outbase } = currentBuild.initialOptions
+      /** Pino and worker */
+      const customEntrypoints: Record<string, string> = {
         'thread-stream-worker': path.join(threadStream, 'lib/worker.js'),
         'pino-worker': path.join(pino, 'lib/worker.js'),
         'pino-pipeline-worker': path.join(pino, 'lib/worker-pipeline.js'),
         'pino-file': path.join(pino, 'file.js')
       }
-      const transportsEntrypoints = Object.fromEntries(
-        (transports || []).map((t) => [t, require.resolve(t)])
+      /** Transports */
+      const transportsEntrypoints: Record<string, string> = Object.fromEntries(
+        transports.map((transport) => [transport, require.resolve(transport)])
       )
-      currentBuild.initialOptions.entryPoints = {
-        ...entrypoints,
-        ...customEntrypoints,
-        ...transportsEntrypoints
+
+      let newEntrypoints: NewEntrypointsType = []
+      /** Array of string */
+      if (isStringArray(entryPoints)) {
+        newEntrypoints = transformToNewEntryPointsType({
+          ...transformToObject(entryPoints, outbase),
+          ...customEntrypoints,
+          ...transportsEntrypoints
+        })
+        /** Array of object */
+      } else if (Array.isArray(entryPoints)) {
+        newEntrypoints = [
+          ...(entryPoints as unknown as NewEntrypointsType),
+          ...transformToNewEntryPointsType({
+            ...customEntrypoints,
+            ...transportsEntrypoints
+          })
+        ]
+        /** Object */
+      } else {
+        newEntrypoints = transformToNewEntryPointsType({
+          ...(entryPoints as Record<string, string>),
+          ...customEntrypoints,
+          ...transportsEntrypoints
+        })
       }
+
+      currentBuild.initialOptions.entryPoints = newEntrypoints
 
       let pinoBundlerRan = false
 
